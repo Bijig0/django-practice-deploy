3a59c9342d0d43f5303981cd3bb073124debb109
Bijig0 <76978374+Bijig0@users.noreply.github.com>
2022-02-20 13:45:58 +1100

    'test'

--- django_project/settings.py
+++ django_project/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'djangostaticfiles')
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'blog-home'
